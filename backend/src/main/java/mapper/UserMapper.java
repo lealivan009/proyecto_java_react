adce899f9635c114af3051247cf6616c383af17c
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-06-05 18:17:06 -0300

    mapper de dto a User

---
+++ backend/src/main/java/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package mapper;
+
+import dto.request.UserDtoRegister;
+import models.User;
+
+public class UserMapper {
+    public static User dtoToUser(UserDtoRegister register){
+        return User.builder()
+            .email(register.email())
+            .dni(register.dni())
+            .photo(register.photo())
+            .name(register.name())
+            .lastname(register.lastname())
+            .build();
+    }    
+}

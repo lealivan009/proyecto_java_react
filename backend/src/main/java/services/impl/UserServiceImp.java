02ffa64fda1a80e30149c3e81059328d77ea3635
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-06-06 23:04:57 -0300

    crear login de user y refactorizar validator para varios dto de usuario

--- backend/src/main/java/services/impl/UserServiceImp.java
+++ backend/src/main/java/services/impl/UserServiceImp.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import Exception.UserException;
+import dto.request.UserDtoLogin;
 import dto.request.UserDtoRegister;
 import dto.response.FullUserDto;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -28,13 +29,24 @@ public class UserServiceImp implements UserService {
     @Transactional
     @Override
     public void registerAndSave(UserDtoRegister userRegister) throws Exception {
-        validateUserRegister(userRegister);
+        validateUser(userRegister);
         Optional<User> userEntity = userRepo.findByEmail(userRegister.email());
 
         if (userEntity.isPresent())
             throw new UserException("User with email [" + userRegister.email() + "] is already exist!");
 
         userRepo.persist(UserMapper.dtoToUser(userRegister));
+    }
+    
+    @Override
+    public FullUserDto loginUser(UserDtoLogin userLogin) throws Exception {
+        validateUser(userLogin);
+
+        User userEntity = userRepo.findByEmail(userLogin.email()).orElseThrow(()-> new Exception("email o contraseña incorrectos"));
+        if(!userEntity.getPassword().equals(userLogin.password()))
+            throw new Exception("email o contraseña incorrectos");
+
+        return UserMapper.userToDto(userEntity);
     }
 
     public List<FullUserDto> findAll() {
@@ -52,9 +64,9 @@ public class UserServiceImp implements UserService {
         throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
     }
 
-    // valida los campos del usuario a registrar, lanza una Exception con los campos incorrectos
-    private void validateUserRegister(UserDtoRegister userRegister) throws Exception {
-        var contrains = validator.validate(userRegister);
+    // valida los campos del usuario, lanza una Exception con los campos incorrectos
+    private void validateUser(Object obj) throws Exception {
+        var contrains = validator.validate(obj);
         if (!contrains.isEmpty()) {
             StringBuilder errorsMessage = new StringBuilder();
             contrains.stream()

7149b3c108def8785598f5de17b41d3e86d3bf13
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-06-06 23:00:52 -0300

    entidad a dto en User mapper

--- backend/src/main/java/mapper/UserMapper.java
+++ backend/src/main/java/mapper/UserMapper.java
@@ -1,17 +1,30 @@
 package mapper;
 
 import dto.request.UserDtoRegister;
+import dto.response.FullUserDto;
 import models.User;
 
 public class UserMapper {
-    public static User dtoToUser(UserDtoRegister register){
+    public static User dtoToUser(UserDtoRegister register) {
         return User.builder()
-            .email(register.email())
-            .password(register.password())
-            .dni(register.dni())
-            .photo(register.photo())
-            .name(register.name())
-            .lastname(register.lastname())
-            .build();
-    }    
+                .email(register.email())
+                .password(register.password())
+                .dni(register.dni())
+                .photo(register.photo())
+                .name(register.name())
+                .lastname(register.lastname())
+                .build();
+    }
+
+    public static FullUserDto userToDto(User entity) {
+        return new FullUserDto(
+            entity.getId(),
+            entity.getEmail(), 
+            entity.getPassword(), 
+            entity.getPhoto(),
+            entity.getName(), 
+            entity.getLastname(), 
+            entity.getDni()
+        );
+    }
 }

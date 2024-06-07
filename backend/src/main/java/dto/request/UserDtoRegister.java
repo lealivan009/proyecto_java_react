baf7877fd30f04fa63b68374889b3dbeb803f16a
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-06-06 22:23:16 -0300

    se agrego dependencia hibernate-validator, validar campos usuario a registrar

--- backend/src/main/java/dto/request/UserDtoRegister.java
+++ backend/src/main/java/dto/request/UserDtoRegister.java
@@ -1,12 +1,14 @@
 package dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserDtoRegister(
-    String email,
-    String password,
+    @NotBlank(message = "El email es obligatorio") String email,
+    @NotBlank(message = "Una contraseña es obligatoria") @Size(min = 8, message = "Contraseña con minimo 8 caracteres") String password,
+    @NotBlank(message = "Su nombre es obligatorio") String name,
+    @NotBlank(message = "Su apellido es obligatorio") String lastname,
+    @NotBlank(message = "El numero de dni es obligatorio") String dni,
     String photo,
-    String name,
-    String lastname,
-    String dni,
-    String medicalInsurance,
     int age
 ) {}
--- backend/src/main/java/services/impl/UserServiceImp.java
+++ backend/src/main/java/services/impl/UserServiceImp.java
@@ -10,6 +10,7 @@ import dto.response.FullUserDto;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Validator;
 import mapper.UserMapper;
 import models.User;
 import repositories.UserRepository;
@@ -19,26 +20,30 @@ import services.UserService;
 public class UserServiceImp implements UserService {
 
     @Inject
-    private UserRepository userRepo; 
+    private UserRepository userRepo;
+
+    @Inject
+    Validator validator;
 
     @Transactional
     @Override
     public void registerAndSave(UserDtoRegister userRegister) throws Exception {
+        validateUserRegister(userRegister);
         Optional<User> userEntity = userRepo.findByEmail(userRegister.email());
 
-        if(userEntity.isPresent())
-            throw new UserException("User with email ["+userRegister.email()+"] is already exist!");
-    
-       userRepo.persist(UserMapper.dtoToUser(userRegister));
+        if (userEntity.isPresent())
+            throw new UserException("User with email [" + userRegister.email() + "] is already exist!");
+
+        userRepo.persist(UserMapper.dtoToUser(userRegister));
     }
-    
-    public List<FullUserDto> findAll(){
+
+    public List<FullUserDto> findAll() {
         return userRepo.findAll().project(FullUserDto.class).list();
     }
 
     @Override
     public User findUserById(UUID id) throws Exception {
-        return userRepo.findByIdOptional(id).orElseThrow(()-> new Exception("User not exist with id [ "+id+"]"));
+        return userRepo.findByIdOptional(id).orElseThrow(() -> new Exception("User not exist with id [ " + id + "]"));
     }
 
     @Override
@@ -47,5 +52,15 @@ public class UserServiceImp implements UserService {
         throw new UnsupportedOperationException("Unimplemented method 'updateUser'");
     }
 
-    
+    // valida los campos del usuario a registrar, lanza una Exception con los campos incorrectos
+    private void validateUserRegister(UserDtoRegister userRegister) throws Exception {
+        var contrains = validator.validate(userRegister);
+        if (!contrains.isEmpty()) {
+            StringBuilder errorsMessage = new StringBuilder();
+            contrains.stream()
+                    .forEach(c -> errorsMessage.append(c.getMessageTemplate()).append(", "));
+            throw new Exception(errorsMessage.toString());
+        }
+    }
+
 }

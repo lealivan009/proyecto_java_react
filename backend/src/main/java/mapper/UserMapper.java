69616e596209d17690c5334ee20f04077030442a
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-06-06 22:19:38 -0300

    fix UserMapper

--- backend/src/main/java/mapper/UserMapper.java
+++ backend/src/main/java/mapper/UserMapper.java
@@ -7,6 +7,7 @@ public class UserMapper {
     public static User dtoToUser(UserDtoRegister register){
         return User.builder()
             .email(register.email())
+            .password(register.password())
             .dni(register.dni())
             .photo(register.photo())
             .name(register.name())

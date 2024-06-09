e90d2b1c7513bf785544a7cfb316ad02d133b29d
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-06-09 17:25:06 -0300

    fix dtos prescription y user

--- backend/src/main/java/dto/request/PrescriptionDto.java
+++ backend/src/main/java/dto/request/PrescriptionDto.java
@@ -1,5 +1,6 @@
 package dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -7,5 +8,7 @@ public record PrescriptionDto(
     @NotBlank(message = "Patient name is required!") String patientName,
     @NotBlank(message = "Medication description is required!") String medicamentDesciption,
     @NotBlank(message = "Treatment steps are required!") String posology,
-    @NotNull(message = "Number of days of treatment required!") int durationTratament
+    @NotNull(message = "Number of days of treatment required!") 
+    @Min(value = 1, message = "Value minimus is 1 day") 
+    int durationTratament
 ) { }
--- backend/src/main/java/dto/request/UserDtoUpdate.java
+++ backend/src/main/java/dto/request/UserDtoUpdate.java
@@ -3,12 +3,14 @@ package dto.request;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserDtoUpdate(
     String name, 
     String lastname, 
     @Pattern(regexp = "\\p{Alpha}*@\\p{Alpha}{2,}\\.\\p{Alpha}{2,4}", message = "Email not valid")
-    String email, 
+    String email,
+    @Size(min = 8, message = "Password with minimum 8 characters") 
     String password, 
     @Pattern(regexp = "[0-9]*", message = "Only numbers without points") 
     String dni,

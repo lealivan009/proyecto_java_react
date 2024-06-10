33cbf1818a3d610bd035c02ca09f4a0598e8e61d
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-06-09 23:29:08 -0300

    renombro a exceptions y ErrorDtoResponse

---
+++ backend/src/main/java/exceptions/ErrorDtoResponse.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+import jakarta.ws.rs.core.Response.Status;
+//genera un objeto respuesta en el cuerpo del Response
+public record ErrorDtoResponse(
+    Status httpStatus,
+    int codeError, 
+    String message
+) {}

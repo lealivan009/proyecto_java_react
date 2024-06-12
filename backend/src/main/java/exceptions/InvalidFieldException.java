a5400e74b350b5c679825e3096fa2d0d29e72dad
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-06-12 17:23:53 -0300

    create InvalidFieldException para las validaciones de campos de entrada

---
+++ backend/src/main/java/exceptions/InvalidFieldException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class InvalidFieldException extends Exception {
+    public InvalidFieldException(String msg){
+        super(msg);
+    }
+}
--- backend/src/main/java/validator/Validator.java
+++ backend/src/main/java/validator/Validator.java
@@ -1,5 +1,6 @@
 package validator;
 
+import exceptions.InvalidFieldException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -9,13 +10,13 @@ public class Validator {
     @Inject
     jakarta.validation.Validator validator;
 
-    public void validate(Object objToValidate) throws Exception {
+    public void validate(Object objToValidate) throws InvalidFieldException {
         var contrains = validator.validate(objToValidate);
         if (!contrains.isEmpty()) {
             StringBuilder errorsMessage = new StringBuilder();
             contrains.stream()
                     .forEach(c -> errorsMessage.append(c.getMessageTemplate()).append(", "));
-            throw new Exception(errorsMessage.toString());
+            throw new InvalidFieldException(errorsMessage.toString());
         }
     }
 }

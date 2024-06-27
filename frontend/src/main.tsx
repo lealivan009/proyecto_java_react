c6a4ce5e2beda22b9a7feb0c790883c670e8520e
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-06-26 23:01:15 -0300

    creo tabla de usuarios

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { AxiosInterceptor } from './interceptors/axios.interceptor.tsx';
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+
+const router = createBrowserRouter([
+  { 
+    path: "/",
+    element: <App/>
+  }
+])
+
+AxiosInterceptor();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

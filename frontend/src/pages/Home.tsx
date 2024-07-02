70dfe981478b3663873985dedccec7a59a6bf5fa
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-07-02 18:53:22 -0300

    creo home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,29 @@
-import Register from "./Register";
 
-export const Home = () => {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { User } from "../models/user.models";
+import { getUserById } from "../services/user.service";
+import { Container } from "@mui/material";
+
+export default function Home(){
+  const [user, setUser] = useState<User|null>(null);
+  const { id } = useParams<string>();
+
+  useEffect(() =>{
+    const fetchData = async () =>{
+      try {
+        const user = await getUserById(id)
+        setUser(user)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
-    <Register />
+    <Container>
+      <div>{user?.email}</div>
+    </Container>
   );
 }

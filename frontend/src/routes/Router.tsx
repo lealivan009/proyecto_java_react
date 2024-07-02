18387ec5ea4484d10aaa8e67c4e34330c1d1c3b1
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-07-02 18:53:01 -0300

    creo error page y cambio router

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -6,7 +6,6 @@ import {
 } from "react-router-dom";
 
 //pages
-import { Home } from "../pages/Home";
 import Appointments from "../pages/Appointments";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -14,19 +13,26 @@ import RegisterMedical from "../pages/RegisterMedical";
 import CreateUpdateAppointment from "../pages/CreateUpdateAppointment";
 import Medicals from "../pages/Medicals";
 import MedicalScheludes from "../pages/MedicalScheludes";
-import NavBar from "../pages/NavBar";
+import Home from "../pages/Home";
+import MainLayaout from "../pages/MainLayaout";
+import AccountInLayaout from "../pages/AccountInLayaout";
+import { ErrorPage } from "../pages/ErrorPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<NavBar /> }>
-      <Route path="home" element={<Home />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
+    <Route path="/" element={<MainLayaout />}>
+      <Route path="account" element={<AccountInLayaout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+
+      <Route path="home/:id" element={<Home />} />
       <Route path="my-appointments" element={<Appointments />} />
       <Route path="medical-schedules" element={<MedicalScheludes />} />
       <Route path="medicals" element={<Medicals />} />
       <Route path="create-update-appointments" element={<CreateUpdateAppointment />} />
       <Route path="register-medical" element={<RegisterMedical />} />
+      <Route path="*" element={<ErrorPage />} />
     </Route>
   )
 );

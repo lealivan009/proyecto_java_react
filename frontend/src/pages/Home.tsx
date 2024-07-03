85395849a35d43fd5ece507e494db88b232e19f2
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-07-03 14:05:56 -0300

    traigo user a home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,9 @@
 
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { User } from "../models/user.models";
 import { getUserById } from "../services/user.service";
 import { Container } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Home(){
   const [user, setUser] = useState<User|null>(null);

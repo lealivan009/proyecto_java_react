41791395bcd379cc701e853544f56826b32720fd
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-07-06 18:41:23 -0300

    fix user service y date format

--- frontend/src/utils/date-formatter.ts
+++ frontend/src/utils/date-formatter.ts
@@ -5,9 +5,14 @@ export function dateFormat(date: Date){
     return `${year}-${month}-${day}`;
 } 
 
-export function validateSchedule(startTime, endTime, consultingDate){
+interface ValidateScheduleProps {
+    startTime: string,
+    endTime: string,
+    consultingDate: Date,
+}
+
+export function validateSchedule({startTime, endTime, consultingDate}: ValidateScheduleProps){
     const start = new Date(startTime);
     const end = new Date(endTime);
     return (consultingDate <= start && consultingDate >= end);
-
 }

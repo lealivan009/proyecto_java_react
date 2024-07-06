82455ce9e51518efb030856c2e58cc54e519b250
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-07-06 17:52:46 -0300

    creo interceptor de axios

--- frontend/src/services/axios.config.ts
+++ frontend/src/services/axios.config.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
+import { getValidationError } from "../utils/validation-errors";
 
 export const axiosInstance = axios.create({
     baseURL:"http://localhost:8080/api"
 })
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      console.log("axios error", getValidationError(error.code));  
+      return Promise.reject(error);
+    }
+  );
